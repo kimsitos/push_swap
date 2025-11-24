9ed376b745872503a6cc5bc492cf2aa631985038
Stefan Adrian Cozaciuc <stcozaci@c2r7s2.42madrid.com>
2025-11-24 12:34:34 +0100

    main added

--- main.c
+++ main.c
@@ -6,29 +6,32 @@
 /*   By: stcozaci <stcozaci@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 16:36:35 by stcozaci          #+#    #+#             */
-/*   Updated: 2025/11/21 17:00:56 by stcozaci         ###   ########.fr       */
+/*   Updated: 2025/11/24 12:31:52 by stcozaci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char *argv[])
-{
-	int	i;
-	int	number;
+// int	main(int argc, char *argv[])
+// {
+// 	int	i;
+// 	int	*number;
 
-	i = 1;
-	while (i <= argc && argv[i])
-	{
-		while (argv[i])
-		if (ft_isdigit(number + '0'))
-		{
-			number = ft_atoi(argv[i]);
-			ft_printf("YES!!, %d is a number\n", number);
-		}
-		else
-			ft_printf("no :(, %s its not a number\n", argv[i]);
-		i++;
-	}
-	return (0);
-}
+// 	i = 1;
+// 	number = malloc (sizeof(int) * (argc + 1));
+// 	while (argc >= i && argv[i])
+// 	{
+// 		if (!is_number(argv[i]))
+// 		{
+// 			ft_printf("%s can't be registered as a number, sorry\n", argv[i]);
+// 			free (number);
+// 			return (0);
+// 		}
+// 		number[i - 1] = ft_atoi(argv[i]);
+// 		ft_printf("%d is registered as a number\n", number[i - 1]);
+// 		i++;
+// 	}
+// 	number[argc] = '\0';
+// 	free(number);
+// 	return (0);
+// }
